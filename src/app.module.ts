f2126c8a014c5edd115ce064b6e35f7b6cac2a31
Jovakinn <max05012004@gmail.com>
2022-06-08 23:39:37 +0300

    add new services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     ProductModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION') as 'postgres',
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + 'dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      }),
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://jovakinn:root@cluster0.endkw.mongodb.net/?retryWrites=true&w=majority',
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
